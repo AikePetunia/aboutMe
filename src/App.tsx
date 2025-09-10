6d5a515c1efac61e611f25b04b6bbfb47555cb98
Aike <dvhb47487963@gmail.com>
2025-09-10 23:50:39 +0000

    coming soon...

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Coming soon...</h1>
+        <h6>anyways:</h6>
+        <div>
+          <section className="social-nav">
+            <div className="container-fluid justify-content-center">
+              <a
+                href="https://www.instagram.com/aike.milanesa/"
+                className="nav-link social-media"
+              >
+                <p>instagram</p>
+              </a>
+              <a
+                href="https://twitter.com/AikePetunia"
+                className="nav-link social-media"
+              >
+                <p>x</p>
+              </a>
+              <a
+                href="https://github.com/AikePetunia"
+                className="nav-link social-media"
+              >
+                <p>github</p>
+              </a>
+            </div>
+          </section>
+        </div>
       </header>
     </div>
   );
