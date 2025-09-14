2a126b580a8cab3348689aa34cda77d996381a86
Aike <dvhb47487963@gmail.com>
2025-09-14 02:26:39 +0000

    lanyard, structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,9 @@
-import "./App.css";
+import "./styles/App.css";
 
 export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Coming soon...</h1>
-        <h6>anyways:</h6>
-        <div>
-          <section className="social-nav">
-            <div className="container-fluid justify-content-center">
-              <a
-                href="https://www.instagram.com/aike.milanesa/"
-                className="nav-link social-media"
-              >
-                <p>instagram</p>
-              </a>
-              <a
-                href="https://twitter.com/AikePetunia"
-                className="nav-link social-media"
-              >
-                <p>x</p>
-              </a>
-              <a
-                href="https://github.com/AikePetunia"
-                className="nav-link social-media"
-              >
-                <p>github</p>
-              </a>
-            </div>
-          </section>
-        </div>
-      </header>
+    <div className="grid">
+      <p> hola </p>
     </div>
   );
 }
---
+++ src/hooks/useLanyard.ts
@@ -0,0 +1,13 @@
+export const lanyardLink =
+  "https://api.lanyard.rest/v1/users/433637449307127822";
+
+export async function fetchLanyard() {
+  const res = await fetch(lanyardLink);
+  if (!res.ok) throw new Error("error with lanyard");
+  const json = await res.json;
+  return json;
+}
+
+fetch(lanyardLink)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
