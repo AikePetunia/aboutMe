57c01d24bf884aa9a7b3d940b4d856c98246c9d7
Aike <dvhb47487963@gmail.com>
2025-10-13 10:22:17 +0000

    first build (v 0.1.0)

--- src/hooks/useLanyard.ts
+++
@@ -1,6 +0,0 @@
-const lanyardLink = "https://api.lanyard.rest/v1/users/433637449307127822";
-
-export async function fetchLanyard() {
-  const res = await fetch(lanyardLink);
-  return await res.json();
-}
