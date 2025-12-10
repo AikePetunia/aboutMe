e804bdcfad2bc67643d4881bf1a0664b035e2d73
Aike <dvhb47487963@gmail.com>
2025-12-09 22:29:10 -0300

    styles for steam recent games

--- src/components/leftColumn/currentlyPlaying/CurrentlyPlaying.tsx
+++ src/components/leftColumn/currentlyPlaying/CurrentlyPlaying.tsx
@@ -1,15 +1,34 @@
+import React from "react";
 import { fetchSteam } from "../../../hooks/useSteam";
-
+import "./CurrentlyPlaying.css";
 export function CurrentlyPlaying() {
-  fetchSteam().then((data) => {
-    console.log(data);
-  });
+  const [steamData, setSteamData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetchSteam()
+      .then((data) => {
+        setSteamData(data);
+        console.log("Fetched Steam data:", data);
+      })
+      .catch((error) => {
+        console.error("Error fetching Steam data:", error);
+      });
+  }, []);
+
+  let gamePrefix = "https://store.steampowered.com/app/";
+
   return (
-    <>
-      <p>
-        que estoy jugando actualmete, y si no, mis juegos recientes? fav? !!ç
-      </p>
-    </>
+    <div className="games-grid">
+      {steamData?.games?.map((game) => (
+        <div key={game.appId} className="game-card">
+          <img src={game.icon} alt={game.name} className="game-icon" />
+          <div className="game-info-container">
+            <a href={gamePrefix + game.appId}>{game.name}</a>
+            <p>{game.playtimeHours.toFixed(0)} hours played</p>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
 
